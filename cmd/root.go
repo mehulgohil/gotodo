966e0311b526a03c2ff0596432929d1f3e353e57
Mehul Gohil <mehulgohil75@gmail.com>
2023-06-15 17:50:29 +0530

    persistent flag usage

--- cmd/root.go
+++ cmd/root.go
@@ -4,16 +4,25 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var verbosePersistentFlag bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gotodo",
 	Short: "A todo cli application",
 	Long:  ``,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if verbosePersistentFlag {
+			fmt.Println("Verbose mode is enabled")
+		}
+		return nil
+	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -34,9 +43,9 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gotodo.yaml)")
+	rootCmd.PersistentFlags().BoolVar(&verbosePersistentFlag, "verbose", false, "enable for verbose mode")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
